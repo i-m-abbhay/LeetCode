60b4bd7caedb82f2d894cf9481638d23d71a85c2
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-07-31 21:06:53 +0900

    Time: 0 ms (100%), Space: 7.9 MB (53.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-    if (n <= 0) return false;
-    return !(n & (n - 1));
+
+    //recursive solution
+    bool isPowerOfTwo(int n){
+        
+        if(n==1) return true;
+        if(n==0 || n%2 != 0) return false;
+        return isPowerOfTwo(n/2);
+
+    // bool isPowerOfTwo(int n) {
+    // if (n <= 0) return false;
+    // return !(n & (n - 1));
 }
 
 };
