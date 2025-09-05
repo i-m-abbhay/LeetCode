02b1cfd0ade3684ff5b7541f1c51f09f4a442344
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-09-05 16:17:44 +0900

    Time: 0 ms (100%), Space: 8.7 MB (70.36%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string part;
+        for (char c : s) {
+            if (c == ' ') {
+                words.push_back(part);
+                part.clear();
+            } else {
+                part += c;
+            }
+        }
+
+        words.push_back(part);
+
+        if (words.size() != pattern.size()) return false;
+
+        unordered_map<char, string> p2w;
+        unordered_map<string, char> w2p;
+
+
+        for(int i=0;i<pattern.size();i++){
+            char p = pattern[i];
+            const string& w = words[i];
+            if(p2w.count(p) && p2w[p]!= w) return false;
+            if(w2p.count(w) && w2p[w]!= p) return false;
+            p2w[p] = w;
+            w2p[w]= p;
+        }
+        return true;
+    }
+};
