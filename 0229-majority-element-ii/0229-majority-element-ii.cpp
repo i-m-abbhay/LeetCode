432c812a1cb9fc282b1e87878abaf018f680ec34
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-10-06 10:37:59 +0900

    Time: 0 ms (100%), Space: 23.5 MB (95.44%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int, int> mp;
-        for(auto num: nums){
-            mp[num]++;
+        // map<int, int> mp;
+        // for(auto num: nums){
+        //     mp[num]++;
+        // }
+        // int freq = nums.size()/3;
+        // vector<int> ans;
+        // for(auto el: mp){
+        //     if(el.second>freq) ans.push_back(el.first);
+        // }
+        // return ans;
+
+        //Boyer-Moore Majority Voting Solution
+        int count1 = 0, count2 = 0;
+        int candidate1 = 0,  candidate2 = 0;
+
+        for(int i=0;i<nums.size();i++){
+            if(count1 == 0 && nums[i] != candidate2){
+                count1 = 1;
+                candidate1 = nums[i];
+            }
+            else if(count2 ==0 && nums[i]!=candidate1){
+                count2 = 1;
+                candidate2 = nums[i];
+            }
+            else if (candidate1 == nums[i]) count1++;
+            else if (candidate2 == nums[i]) count2++;
+            else{
+                count1--;
+                count2--;
+            }
         }
-        int freq = nums.size()/3;
-        vector<int> ans;
-        for(auto el: mp){
-            if(el.second>freq) ans.push_back(el.first);
+        vector<int> result;
+        int threshold = nums.size()/3;
+        count1 = 0; count2 = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(candidate1 == nums[i]) count1++;
+            else if(candidate2 == nums[i]) count2++;
         }
-        return ans;
+        if(count1>threshold) result.push_back(candidate1);
+        if(count2>threshold) result.push_back(candidate2);
+
+        return result;
     }
 };
