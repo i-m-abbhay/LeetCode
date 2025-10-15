f35cc79bd8c18015482f88a17ff3fc8c0fca3448
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-10-15 13:18:59 +0900

    Time: 0 ms (100%), Space: 28.2 MB (28.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int fq =1;
+        int num = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(num == nums[i]) fq++;
+            else if (fq == 0){
+                num = nums[i];
+                fq++;
+            }
+            else{
+                fq--;
+            }
         }
-        vector<pair<int,int>> vec(mp.begin(), mp.end());
-        std::sort(vec.begin(), vec.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-        return a.second < b.second;
-        });
-        return vec[mp.size()-1].first;
+        return num;
     }
 };
