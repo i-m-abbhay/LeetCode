e23947fa4ae041a1af1ea7c6f59a4a35509122f2
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-10-23 09:02:14 +0900

    Time: 3 ms (88.34%), Space: 17.9 MB (39.8%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,15 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
-            return False
-        if x==0:
-            return True
-        numbers = []
-        flg = True
-        while(x):
-            numbers.append(x%10)
-            x=x//10
-        for i in range(len(numbers)):
-            if(numbers[i]!=numbers[len(numbers)-i-1]):
-                return False
-        return flg
+        # if x<0:
+        #     return False
+        # if x==0:
+        #     return True
+        # numbers = []
+        # flg = True
+        # while(x):
+        #     numbers.append(x%10)
+        #     x=x//10
+        # for i in range(len(numbers)):
+        #     if(numbers[i]!=numbers[len(numbers)-i-1]):
+        #         return False
+        # return flg
+        s=str(x)
+        return s == s[::-1]
