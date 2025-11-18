2f65766a94729cb5abfbe9634b0135ab66e5cee7
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-11-18 15:31:55 +0900

    Time: 327 ms (35.22%), Space: 101 MB (63.04%) - LeetHub

--- 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.js
+++ 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.js
@@ -4,19 +4,48 @@
  * @return {Array}
  */
 var join = function(arr1, arr2) {
-    const map = new Map();
-    for(const obj of arr1){
-        map.set(obj.id, {...obj});
-    }
-    for(const obj of arr2){
-        if(map.has(obj.id)){
-            map.set(obj.id, {
-                ...map.get(obj.id), ...obj
-            });
+    arr1.sort((a,b)=>a.id-b.id);
+    arr2.sort((a,b)=>a.id-b.id);
+    const res = [];
+    let l = 0, r = 0;
+    while(l<arr1.length && r<arr2.length){
+        const o1 = arr1[l];
+        const o2 = arr2[r];
+
+        if(o1.id === o2.id){
+            res.push({...o1, ...o2});
+            l++;
+            r++;
         }
-        else{
-            map.set(obj.id, {...obj});
+        else if(o1.id<o2.id){
+            res.push({...o1});
+            l++;
+        }
+        else {
+            res.push({...o2});
+            r++;
         }
     }
-    return Array.from(map.values()).sort((a,b)=> a.id - b.id);
+    while(l<arr1.length) res.push({...arr1[l++]});
+    while(r<arr2.length) res.push({...arr2[r++]});
+    return res;
+
 };
+
+// var join = function(arr1, arr2) {
+//     const map = new Map();
+//     for(const obj of arr1){
+//         map.set(obj.id, {...obj});
+//     }
+//     for(const obj of arr2){
+//         if(map.has(obj.id)){
+//             map.set(obj.id, {
+//                 ...map.get(obj.id), ...obj
+//             });
+//         }
+//         else{
+//             map.set(obj.id, {...obj});
+//         }
+//     }
+//     return Array.from(map.values()).sort((a,b)=> a.id - b.id);
+// };
