46b131eae6181646263625d0149b893dc7ecae3d
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-12-06 14:51:15 +0900

    Time: 4 ms (12%), Space: 71.6 MB (81.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,43 @@
 class Solution {
+private:
+    //*******BRUTE FORCE*****
+    // int sum(vector<int> &nums, int s, int e){
+    //     int sum = 0;
+    //     for(int i = s; i<=e; i++){
+    //         sum+=nums[i];
+    //     }
+    //     return sum;
+    // }
 public:
     int maxSubArray(vector<int>& nums) {
-       int prefixSum = 0;
-       int minPrefix = 0;
-       int maxSum = INT_MIN;
-       for(auto num: nums){
-            prefixSum = prefixSum+num;
-            int candidate = prefixSum - minPrefix;
-            if(candidate>maxSum) maxSum = candidate;
-            if(prefixSum<minPrefix) minPrefix = prefixSum;
-       }
-        return maxSum;
+        //****BRUTE FORCE***** O N3
+        // int maxSum = INT_MIN;
+        // for(int i = 0; i < nums.size(); i++){
+        //     for(int j = i; j<nums.size();j++){
+        //         maxSum = max(maxSum, sum(nums,i,j));
+        //     }
+        // }
+        // return maxSum;
+
+        //******BETTER******* O N2
+        // int maxSum = INT_MIN;
+        // for(int i=0;i<nums.size(); i++){
+        //     int sum = 0;
+        //     for(int j=i; j<nums.size();j++){
+        //         sum+=nums[j];
+        //         maxSum = max(sum,maxSum);
+        //     }
+            
+        // }
+        // return maxSum;
+
+        //*****Optimal (Kadane's Algorithm)*******
+        int maxi = nums[0];
+        int sum = nums[0];
+        for(int i = 1; i<nums.size();i++){ // simple do not carry forward the sum which hampers next element
+            sum = max(nums[i], sum + nums[i]);
+            maxi = max(maxi, sum);
+        }
+        return maxi;
     }
 };
