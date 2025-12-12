546fe54cbc53e106f5f029cadeff4a666307d730
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-12-13 08:49:39 +0900

    Time: 0 ms (100%), Space: 9.4 MB (50.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,16 @@
 class Solution {
+private:
+    int move(int row, int col, int m, int n, vector<vector<int>>& dp){
+        if(row==m-1 && col == n-1) return 1;
+        if(row>=m || col >= n) return 0;
+        if(dp[row][col]!=-1) return dp[row][col];
+        int right = move(row, col+1, m, n, dp);
+        int down = move(row+1, col, m, n, dp);
+        return dp[row][col] = right+down;
+    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return pathHelper(0, 0, m-1, n-1, dp);
-    }
-    int pathHelper(int currRowInd, int currColInd, int maxRowInd, int maxColInd, vector<vector<int>>& dp){
-        if(currRowInd==maxRowInd && currColInd == maxColInd) return 1;
-        if (currRowInd>maxRowInd || currColInd > maxColInd) return 0;
-        if(dp[currRowInd][currColInd]!=-1) return dp[currRowInd][currColInd];
-        int right = pathHelper(currRowInd, currColInd+1, maxRowInd, maxColInd, dp);
-        int down = pathHelper(currRowInd+1, currColInd, maxRowInd, maxColInd, dp);
-        return dp[currRowInd][currColInd] = right + down;
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        return move(0, 0, m, n, dp);
     }
 };
