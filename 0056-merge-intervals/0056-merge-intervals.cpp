aba640002042e40b06bbff977ee95d79533c7973
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2025-12-15 11:28:06 +0900

    Time: 3 ms (89.99%), Space: 24 MB (34.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,22 @@
 class Solution {
+
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> v;
+        if(intervals.empty()) return {};
+        vector<vector<int>> res;
         sort(intervals.begin(), intervals.end());
-        int start = intervals[0][0], end = intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            if(end>=intervals[i][0]){
-                end = max(intervals[i][1], end);
-            }
-            else{
-                v.push_back({start, end});
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i = 0; i<intervals.size();i++){
+            if(intervals[i][0]<=end){
+                end = max(end, intervals[i][1]);
+            } else{
+                res.push_back({start, end});
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-        v.push_back({start, end});
-        return v;
+        res.push_back({start, end});
+        return res;
     }
 };
