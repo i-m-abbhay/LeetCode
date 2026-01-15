233406f5cbf03a782c8bed0e3b7d3600a32aadcd
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-01-15 09:11:54 +0900

    Time: 0 ms (100%), Space: 3.9 MB (99.56%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+    if head==nil {return head}
+    var slow *ListNode = head;
+    var fast *ListNode = head;
+    for fast!=nil && fast.Next != nil{
+        slow=slow.Next;
+        fast = fast.Next.Next;
+    }
+    return slow;
+}
