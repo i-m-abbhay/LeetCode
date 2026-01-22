d84d7c34db0a82340f9f306e5cda3a7b0d512378
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-01-22 16:15:45 +0900

    Time: 3 ms (24.25%), Space: 10.5 MB (79.7%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int numOfBits(int i){
+        int cnt=0;
+        while(i!=0){
+            if(i&1) cnt++;
+            i>>=1;
+        }
+        return cnt;
+    }
+public:
+    vector<int> countBits(int n) {
+        vector<int>res(n+1, 0);
+        for(int i=0;i<n+1;i++){
+            res[i] = numOfBits(i);
+        }
+        return res;
+    }
+};
