4c7ec6e207d5dc60a4e3a11d946d0fcd38dafabf
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-01-22 16:17:52 +0900

    Time: 3 ms (24.25%), Space: 10.7 MB (56.29%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,26 @@
 class Solution {
-private:
-    int numOfBits(int i){
-        int cnt=0;
-        while(i!=0){
-            if(i&1) cnt++;
-            i>>=1;
-        }
-        return cnt;
-    }
+// private:
+//     int numOfBits(int i){
+//         int cnt=0;
+//         while(i!=0){
+//             if(i&1) cnt++;
+//             i>>=1;
+//         }
+//         return cnt;
+//     }
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int>res(n+1, 0);
+//         for(int i=0;i<n+1;i++){
+//             res[i] = numOfBits(i);
+//         }
+//         return res;
+//     }
 public:
-    vector<int> countBits(int n) {
-        vector<int>res(n+1, 0);
-        for(int i=0;i<n+1;i++){
-            res[i] = numOfBits(i);
+    vector<int> countBits(int n){
+        vector<int> res(n+1,0);
+        for(int i = 1; i<=n;i++){
+            res[i] = res[i>>1]+(i&1);
         }
         return res;
     }
