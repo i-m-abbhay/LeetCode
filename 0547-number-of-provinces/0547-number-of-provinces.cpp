92843bc5c3f23b43ecb3928657f57049b28e4ace
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-01-25 15:38:10 +0900

    Time: 1 ms (48.8%), Space: 19.8 MB (51.12%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void bfs(int node, vector<vector<int>>& isConnected, vector<int>& vis){
+        queue<int> q;
+        q.push(node);
+        vis[node] = 1;
+        while(!q.empty()){
+            int vertex = q.front();
+            q.pop();
+            for(int v = 0; v<isConnected.size();v++){
+                if(isConnected[vertex][v]==1&&!vis[v]){
+                    vis[v]=1;
+                    q.push(v);
+                }
+            }
+        }
+
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int numberOfProvinces = 0;
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                bfs(i, isConnected, vis);
+                numberOfProvinces++;
+            }
+        }
+        return numberOfProvinces;
+    }
+};
