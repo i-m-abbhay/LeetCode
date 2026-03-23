da3f70f754ec7b4376dd211c4502c30ce5fe4261
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-03-23 13:53:16 +0900

    Time: 4 ms (21.27%), Space: 62.8 MB (97.47%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int maxArea = INT_MIN;
-        int l = 0, r = height.size()-1;
+        /*
+        we can solve this using two pointer approach
+        anytime the water volume of water will be
+        water = min(height[l], height[r]) * (r-l)
+        we need to maximize the above thing...
+        we can be a little greedy by moving the pointer which has the min height cause moving the bigger doesn't make sense cause the width (r-l) is gonna decreased anyway so it makes sense to move the smaller one inward.
+
+        */
+        int l = 0, r = height.size()-1, bestArea = INT_MIN;
         while(l<r){
-            int area = min(height[l], height[r])*(r-l);
-            maxArea = max(maxArea, area);
+            int area = min(height[l], height[r]) * (r-l);
+            bestArea = max(area, bestArea);
             if(height[l]<height[r]) l++;
             else r--;
         }
-        return maxArea;
+        return bestArea;
     }
 };
