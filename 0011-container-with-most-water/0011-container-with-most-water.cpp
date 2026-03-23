5ab2a2e401c0f09c88956180d819d7e8bee11b9a
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-03-23 13:54:18 +0900

    Time: 0 ms (100%), Space: 62.9 MB (47.89%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -9,7 +9,7 @@ public:
         we can be a little greedy by moving the pointer which has the min height cause moving the bigger doesn't make sense cause the width (r-l) is gonna decreased anyway so it makes sense to move the smaller one inward.
 
         */
-        int l = 0, r = height.size()-1, bestArea = INT_MIN;
+        int l = 0, r = height.size()-1, bestArea = 0;
         while(l<r){
             int area = min(height[l], height[r]) * (r-l);
             bestArea = max(area, bestArea);
