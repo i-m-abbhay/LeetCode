f31a802a399f55ef50a10a0e4ed4f0067049bc74
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-03-26 22:17:56 +0900

    Time: 0 ms (100%), Space: 26 MB (78.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int maxEl=INT_MIN, maxInd=0;
-        for(int i =0;i<height.size();i++){
-            if(height[i]>=maxEl){   
-                maxInd = i;
-                maxEl = height[i];
+        int n = height.size();
+        int l = 0; int r = n - 1;
+        int maxRight = 0;
+        int maxLeft = 0;
+        int water = 0;
+        while(l<r){
+            maxLeft = max(height[l], maxLeft);
+            maxRight = max(height[r], maxRight);
+            if(maxLeft > maxRight){
+                water += maxRight - height[r];
+                r--;
+            }
+            else {
+                water += maxLeft - height[l];
+                l++;
             }
         }
-        int preMax = 0;
-        int preWater = 0; 
-        for(int i=0;i<=maxInd;i++){
-            preMax = max(preMax, height[i]);
-            preWater += preMax - height[i];
-        }
-        int sufWater = 0;
-        int sufMax = 0;
-        for(int i=height.size()-1;i>maxInd;i--){
-            sufMax = max(sufMax, height[i]);
-            sufWater += sufMax - height[i];
-        }
-        return sufWater+preWater;
+        return water;
     }
 };
