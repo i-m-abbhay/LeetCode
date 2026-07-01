fb3f5cfb9548f7b07b932ea9485295791dfcff99
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-07-02 08:12:59 +0900

    Time: 87 ms (11.73%), Space: 113.3 MB (5.27%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> unq;
+        unordered_set<int> unq;
         for(auto num:nums){
             unq.insert(num);
         }
