82196cf4c7646b873b997491f052c445b2d5988c
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-07-03 13:11:17 +0900

    Time: 0 ms (100%), Space: 6 MB (30.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,38 @@
 func twoSum(nums []int, target int) []int {
+    // brute force (On2, O1)
+    // n:= len(nums)
+    // for i:= 0;i<n;i++{
+    //     for j:= i+1; j<n; j++{
+    //         if nums[i] + nums[j] == target{
+    //             return []int{i,j}
+    //         }
+    //     }
+    // }
+    // return nil
+    
+    //Two pass hashmap (On, On)
+    // seen := make(map[int]int)
+    
+    // for i, num := range nums {
+    //     seen[num] = i;
+    // } 
+    
+    // for i, num := range nums{
+    //     complement := target - num
+    //     if index, found := seen[complement]; found{
+    //         return []int{i,index}
+    //     } 
+    // }
+    // return nil
+
+    //One pass hashmap (On, On)
+    seen:= make(map[int]int)
     for i, num := range nums {
-        for j := i+1; j< len(nums); j++ {
-            if target == num + nums[j]{
-                return []int{i,j}
-            }
+        complement := target - num
+        if index, found := seen[complement]; found{
+            return []int{index, i}
         }
+        seen[num]=i
     }
-    return []int{0,0}
+    return nil
 }
