a1af33afcb6d9a27ca2c01fdce7bc6f8796e9efe
Abhay Tiwari <82487231+i-m-abbhay@users.noreply.github.com>
2026-07-14 14:02:44 +0900

    Time: 2 ms (46.85%), Space: 8 MB (57.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -1,16 +1,42 @@
 func topKFrequent(nums []int, k int) []int {
-    count := make(map[int]int)
+    //Sort by frequency
+    // time O(n log n) Space On
+    // count := make(map[int]int)
 
-    for _, n := range nums {
+    // for _, n := range nums {
+    //     count[n]++
+    // }
+    // keys := make([]int, 0, len(count))
+    // for key := range count {
+    //     keys = append(keys, key)
+    // }
+    // sort.Slice(keys, func(i, j int) bool {
+    //     return count[keys[i]]>count[keys[j]]
+    // })
+
+    // return keys[:k]
+
+    //Bucket Sort bucket index = frequency
+    //Time On Space On
+    count := make(map[int]int)
+    for _, n:= range nums {
         count[n]++
     }
-    keys := make([]int, 0, len(count))
-    for key := range count {
-        keys = append(keys, key)
-    }
-    sort.Slice(keys, func(i, j int) bool {
-        return count[keys[i]]>count[keys[j]]
-    })
 
-    return keys[:k]
+    buckets := make([][]int, len(nums)+1)
+    for num, freq := range count {
+        buckets[freq] = append(buckets[freq], num)
+    }
+
+    res := make([]int, 0, k)
+    for f:= len(buckets) - 1; f >= 0 && len(res) < k; f-- {
+        for _, num := range buckets[f]{
+            res = append(res, num)
+            if len(res)==k {
+                break
+            }
+        }
+    }
+    return res
+
 }
